aa6eff41c1876362c606584809285d63f73ae186
Karthikramesh12 <133801320+Karthikramesh12@users.noreply.github.com>
2025-06-17 15:28:13 +0530

    made few changes in index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//this change is made by karthik to see how merge and all the good stuff goes
 reportWebVitals();
